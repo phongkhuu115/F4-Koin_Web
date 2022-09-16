0fed73977086a91846269d7127df734f908c471b
phongkhuu115 <phongkhuu115@gmail.com>
2022-09-16 17:39:09 +0700

    add migrate db

---
+++ f4koin_be/app/Models/Galery.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Galery extends Model
+{
+    use HasFactory;
+}
---
+++ f4koin_be/database/migrations/2022_09_16_103510_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->integer('productID')->primary();
+            $table->string('productName');
+            $table->string('productType');
+            $table->string('productDetail');
+            $table->integer('productPrice');
+            $table->integer('productCategoryID');
+            $table->integer('productInentory');
+            $table->integer('productDiscountID');
+            $table->string('productThumbnail');
+            $table->dateTime('create_at');
+            $table->dateTime('update_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};

c0cf9996f798fa24fb1af32fbcd2e52927587b64
Nguyễn Anh Tài <vipkuteaz1122@gmail.com>
2022-10-26 19:54:46 +0700

    update try catch product controller

--- be_f4koin/app/Http/Controllers/API/ItemController.php
+++ be_f4koin/app/Http/Controllers/API/ItemController.php
@@ -14,11 +14,12 @@ class ItemController extends Controller
     public function customImageUrl($data)
     {
         $data->map(function ($item) {
-            $item->image = 'http://localhost:8000/storage/' . rand(1, 1000);
+            $item->imageUrl = 'https://picsum.photos/500/500?' . rand(1, 1000);
             return $item;
         });
         return $data;
     }
+
     public function isAdmin(Request $request)
     {
         if ($request->user()->userRoleID == 1) {
@@ -30,38 +31,57 @@ class ItemController extends Controller
 
     // get x item random param
     public function getXRandom(Request $request)
-    {       
-        $x = $request->input('x');
-        // return id, name, price, imageurl
+    {
+        try {
+            $x = $request->input('x');
 
-        $items = Product::inRandomOrder()->limit($x)->get(['productID', 'productName', 'productPrice', 'imageUrl']);
-        
+
+            $items = Product::inRandomOrder()->limit($x)->get(['productID', 'productName', 'productPrice', 'imageUrl']);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Something went wrong',
+            ], 500);
+        }
         return response()->json([
             'product' =>  $items = $this->customImageUrl($items),
-             'message' => $items->isEmpty() ? 'product not found' : 'success'
-            ], 200);
+            'message' => $items->isEmpty() ? 'product not found' : 'success'
+        ], 200);
     }
 
     // get 6 items random
     public function get6Random()
     {
-        $items = Product::inRandomOrder()->limit(6)->get(['productID', 'productName', 'productPrice', 'imageUrl']);
+        try {
+            $items = Product::inRandomOrder()->limit(6)->get(['productID', 'productName', 'productPrice', 'imageUrl']);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Something went wrong',
+            ], 500);
+        }
+
         return response()->json([
             'product' =>  $items = $this->customImageUrl($items),
-             'message' => $items->isEmpty() ? 'product not found' : 'success'
-            ], 200);
+            'message' => $items->isEmpty() ? 'product not found' : 'success'
+        ], 200);
     }
     public function get3Latest()
     {
-        // return id, name, price, imageurl
-        $data = Product::select('productID', 'productName', 'productPrice', 'imageUrl')->orderBy('create_at', 'desc')->take(3)->get();
+
+        try {
+            $data = Product::select('productID', 'productName', 'productPrice', 'imageUrl')->orderBy('create_at', 'desc')->take(3)->get();
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Something went wrong',
+            ], 500);
+        }
 
         //  temp modify for MrFong to test Layout
         //  modify image url with 'https://picsum.photos/200/300?'+random(1,1000)
-        foreach ($data as $item) {
-            $item->imageUrl = 'https://picsum.photos/500/500?' . rand(1, 1000);
-        }
-        return response()->json(['product' => $data, 'message:' => $data != null ? 'success' : 'fail'], 200);
+
+        return response()->json([
+            'product' =>  $data = $this->customImageUrl($data),
+            'message:' => $data != null ? 'success' : 'fail'
+        ], 200);
     }
 
     /**
@@ -69,17 +89,23 @@ class ItemController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function getbycategoryid()
-    {
-        $product = Product::where('productCategoryID', request('productCategoryID'))->get();
-        return response()->json($product);
-    }
     public function getbyid(Request $request)
     {
-        $item = Product::find($request->productID);
+        try {
+            $data = Product::where('productID', $request->input('productID'))
+            ->get(['productID', 'productName', 'productPrice', 'imageUrl']);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Something went wrong',
+            ], 500);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Something went wrong',
+            ], 500);
+        }
         return response()->json([
-            'product' => $item,
-            'message' => $item != null ?  'success' : 'fail'
+            'product' =>  $data = $this->customImageUrl($data),
+            'message' => $data != null ?  'success' : 'fail'
         ], 200);
     }
     /**
@@ -90,27 +116,33 @@ class ItemController extends Controller
      */
     public function insert(Request $request)
     {
-        if ($this->isAdmin($request)) {
-            $product = new Product;
-            $product->productName = $request->productName;
-            $product->productType = $request->productType;
-            $product->productDetail = $request->productDetail;
-            $product->productPrice = $request->productPrice;
-            $product->productCategoryID = $request->productCategoryID;
-            $product->productInventory = $request->productInventory;
-            $product->productDiscountID = $request->productDiscountID;
-            $product->productThumbnail = $request->productThumbnail;
-            $product->create_at = now();
-            $product->update_at = now();
+        try {
+            if ($this->isAdmin($request)) {
+                $product = new Product;
+                $product->productName = $request->productName;
+                $product->productType = $request->productType;
+                $product->productDetail = $request->productDetail;
+                $product->productPrice = $request->productPrice;
+                $product->productCategoryID = $request->productCategoryID;
+                $product->productInventory = $request->productInventory;
+                $product->productDiscountID = $request->productDiscountID;
+                $product->productThumbnail = $request->productThumbnail;
+                $product->create_at = now();
+                $product->update_at = now();
+                return response()->json([
+                    'product added' => $request->all(),
+                    'message' =>  $product->save() ? 'Product created successfully' : 'Product created failed',
+                ], 200);
+            } else {
+                return response()->json([
+                    // 'role' => $request->user()->userRoleID,
+                    'message' => 'You have no permission'
+                ], 401);
+            }
+        } catch (\Throwable $th) {
             return response()->json([
-                'product added' => $request->all(),
-                'message' =>  $product->save() ? 'Product created successfully' : 'Product created failed',
-            ], 200);
-        } else {
-            return response()->json([
-                // 'role' => $request->user()->userRoleID,
-                'message' => 'You have no permission'
-            ], 401);
+                'message' => 'Something went wrong',
+            ], 500);
         }
     }
 
@@ -125,7 +157,7 @@ class ItemController extends Controller
         $product = Product::all();
         return response()->json([
             'product' => $product,
-            // 'role' => $request->user()->userRoleID,
+            'message' => $product->isEmpty() ? 'product not found' : 'success'
         ], 200);
     }
 
@@ -138,30 +170,36 @@ class ItemController extends Controller
      */
     public function edit(Request $request)
     {
-        if ($this->isAdmin($request)) {
-            $productFound =  Product::where('productID', $request->productID);
-            return response()->json([
+        try {
+            if ($this->isAdmin($request)) {
+                $productFound =  Product::where('productID', $request->productID);
+                return response()->json([
 
-                'message' =>  $productFound->update([
-                    'productName' => $request->productName,
-                    'productType' => $request->productType,
-                    'productDetail' => $request->productDetail,
-                    'productPrice' => $request->productPrice,
-                    'productCategoryID' => $request->productCategoryID,
-                    'productInventory' => $request->productInentory,
-                    'productDiscountID' => $request->productDiscountID,
-                    'productThumbnail' => $request->productThumbnail,
-                    'update_at' => now()
-                ]) ? 'Product updated successfully' : 'Product update failed',
-                //product updated
-                'product' => $productFound->get()->count() != 0 ? $productFound->get() : 'Product not found',
+                    'message' =>  $productFound->update([
+                        'productName' => $request->productName,
+                        'productType' => $request->productType,
+                        'productDetail' => $request->productDetail,
+                        'productPrice' => $request->productPrice,
+                        'productCategoryID' => $request->productCategoryID,
+                        'productInventory' => $request->productInentory,
+                        'productDiscountID' => $request->productDiscountID,
+                        'productThumbnail' => $request->productThumbnail,
+                        'update_at' => now()
+                    ]) ? 'Product updated successfully' : 'Product update failed',
+                    //product updated
+                    'product' => $productFound->get()->count() != 0 ? $productFound->get() : 'Product not found',
 
-            ], 200);
-        } else {
+                ], 200);
+            } else {
+                return response()->json([
+                    // 'role' => $request->user()->userRoleID,
+                    'message' => 'You have no permission'
+                ], 401);
+            }
+        } catch (\Throwable $th) {
             return response()->json([
-                // 'role' => $request->user()->userRoleID,
-                'message' => 'You have no permission'
-            ], 401);
+                'message' => 'Something went wrong',
+            ], 500);
         }
     }
 
@@ -173,18 +211,24 @@ class ItemController extends Controller
      */
     public function destroy(Request $request)
     {
-        if ($this->isAdmin($request)) {
+        try {
+            if ($this->isAdmin($request)) {
 
-            $productFound = Product::where('productID', $request->productID);
+                $productFound = Product::where('productID', $request->productID);
+                return response()->json([
+                    'product' => $productFound->get()->count() != 0  ? $productFound->get() : 'Product not found',
+                    'message' =>  $productFound->delete() ? 'Product deleted successfully' : 'Product delete failed',
+                ], 200);
+            } else {
+                return response()->json([
+                    // 'role' => $request->user()->userRoleID,
+                    'message' => 'You have no permission'
+                ], 401);
+            }
+        } catch (\Throwable $th) {
             return response()->json([
-                'product' => $productFound->get()->count() != 0  ? $productFound->get() : 'Product not found',
-                'message' =>  $productFound->delete() ? 'Product deleted successfully' : 'Product delete failed',
-            ], 200);
-        } else {
-            return response()->json([
-                // 'role' => $request->user()->userRoleID,
-                'message' => 'You have no permission'
-            ], 401);
+                'message' => 'Something went wrong',
+            ], 500);
         }
     }
 
@@ -193,16 +237,16 @@ class ItemController extends Controller
     {
         try {
             $category = Category::where('categoryName', $request->categoryName)->first();
-            $items = Product::select('productID', 'productName', 'productPrice', 'imageUrl')->where('productCategoryID', $category->categoryID)->get();           
+            $items = Product::select('productID', 'productName', 'productPrice', 'imageUrl')->where('productCategoryID', $category->categoryID)->get();
         } catch (\Throwable $th) {
             return response()->json([
                 'message' => 'Category not found'
             ], 404);
         }
-       
+
         return response()->json([
             'product' =>  $items = $this->customImageUrl($items),
-            'category' => $category,
+            // 'category' => $category,
             'message' => $items->isEmpty() ? 'product not found' : 'success'
         ], 200);
     }
--- be_f4koin/routes/api.php
+++ be_f4koin/routes/api.php
@@ -25,13 +25,13 @@ Route::get('/get6Random', [ItemController::class, 'get6Random']);
 Route::get('/getXRandom', [ItemController::class, 'getXRandom']);
 Route::get('/getAllItem', [ItemController::class, 'index']);
 Route::get('/getItemByCategoryName', [ItemController::class, 'getItemByCategoryName']);
+Route::get('/getSpecifyItem', [ItemController::class, 'getbyid']);
+Route::get('/getCategoryItem', [ItemController::class, 'getbycategoryid']);
 // Route Product
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::put('/updateItem', [ItemController::class, 'edit']);
     Route::post('/insertItem', [ItemController::class, 'insert']);
     Route::delete('/deleteItem', [ItemController::class, 'destroy']);
-    Route::get('/getSpecifyItem', [ItemController::class, 'getbyid']);
-    Route::get('/getCategoryItem', [ItemController::class, 'getbycategoryid']);
 });
 
 // Route User
